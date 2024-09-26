b26b71e6b94c7a07192d8b04ecfe6029a0d50fc8
Alperen941 <alperenozer03@gmail.com>
2024-09-26 22:57:29 +0200

    assignment 2 part 2

--- Server/CLI/UI/CliApp.cs
+++ Server/CLI/UI/CliApp.cs
@@ -1,6 +1,23 @@
-﻿namespace CLI.UI;
+﻿using RepositoryContracts;
+
+namespace CLI.UI;
 
 public class CliApp
 {
-    
+    private IUserRepository iUserRepository{get;set;}
+    private IcommentRepository iCommentRepository{get;set;}
+    private IpostRepository iPostRepository{get;set;}
+
+    public CliApp(IUserRepository iUserRepository, IcommentRepository iCommentRepository,
+        IpostRepository iPostRepository)
+    {
+        this.iUserRepository = iUserRepository;
+        this.iCommentRepository = iCommentRepository;
+        this.iPostRepository = iPostRepository;
+    }
+
+    public async Task StartAsync()
+    {
+        Console.Write("starting cliAPP");
+    }
 }
