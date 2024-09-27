3f204fd41dc448676b46da53987a1d6fd50da7bf
Baris Cankül <147693702+BarisCnkl@users.noreply.github.com>
2024-09-27 09:39:28 +0200

    2.5

---
+++ CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using CLI.UI;
+using InMemoryRepository;
+using RepositoryContracts;
+
+Console.WriteLine("Starting CLI app...");
+IUserRepository userRepository = new UserInMemoryRepository();
+IcommentRepository commentRepository = new CommentInMemoryRepository();
+IpostRepository postRepository = new PostInMemoryRepository();
+ILikeRepository likeRepository = new LikeInMemoryRepository();
+
+CliApp cliApp = new CliApp(userRepository, commentRepository, postRepository, likeRepository);
+await cliApp.StartAsync();
